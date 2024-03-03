2a01e9101d9c5f5ecb1bdb63f104e66fa362aab6
safwandm <safwan.damanik@gmail.com>
2024-03-03 15:36:04 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Submit = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(29, 101);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(67, 23);
+            this.Submit.TabIndex = 0;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Output
+            // 
+            this.Output.AutoSize = true;
+            this.Output.BackColor = System.Drawing.SystemColors.Control;
+            this.Output.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Output.Location = new System.Drawing.Point(26, 32);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(116, 13);
+            this.Output.TabIndex = 1;
+            this.Output.Text = "Masukkan nama anda:";
+            this.Output.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(29, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 147);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Submit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Tugas TP MOD 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
